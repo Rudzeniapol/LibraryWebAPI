a8dbb9faadcde18e3b7ef2be41e5fcd7159cd95b
Rudzeniapol <ffgg94802@gmail.com>
2025-02-27 17:42:00 +0300

    add new exceptions to ExceptionMiddleware.cs and refactor ExceptionMiddleware.cs

--- LibraryAPI/Middlewares/ExceptionMiddleware.cs
+++ LibraryAPI/Middlewares/ExceptionMiddleware.cs
@@ -1,5 +1,6 @@
 ﻿using System.Net;
 using System.Text.Json;
+using FluentValidation;
 using LibraryAPI.Exceptions;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
@@ -28,37 +29,49 @@ namespace LibraryAPI.Middlewares
             {
                 _logger.LogError(ex.Message);
                 context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
-                await context.Response.WriteAsJsonAsync(new { error = ex.Message, code = 400 });
+                await context.Response.WriteAsJsonAsync(new { error = "Невалидный запрос", code = 400, details = ex.Message });
             }
             catch (NotFoundException ex)
             {
                 _logger.LogError(ex.Message);
                 context.Response.StatusCode = (int)HttpStatusCode.NotFound;
-                await context.Response.WriteAsJsonAsync(new { error = ex.Message, code = 404 });
+                await context.Response.WriteAsJsonAsync(new { error = "Сущность не найдена", code = 404, details = ex.Message });
             }
             catch (EntityExistsException ex)
             {
                 _logger.LogError(ex.Message);
                 context.Response.StatusCode = (int)HttpStatusCode.Conflict;
-                await context.Response.WriteAsJsonAsync(new { error = ex.Message, code = 409 });
+                await context.Response.WriteAsJsonAsync(new { error = "Сущность уже существует", code = 409, details = ex.Message });
             }
             catch (SecurityTokenException ex)
             {
                 _logger.LogError(ex.Message);
                 context.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
-                await context.Response.WriteAsJsonAsync(new { error = ex.Message, code = 401 });
+                await context.Response.WriteAsJsonAsync(new { error = "Ошибка токена", code = 401, details = ex.Message });
             }
-            catch (TaskCanceledException e)
+            catch (TaskCanceledException ex)
             {
                 _logger.LogWarning("Истекло время ожидания для запроса");
                 context.Response.StatusCode = (int)HttpStatusCode.RequestTimeout;
-                await context.Response.WriteAsJsonAsync(new { error = "Время ожидания истекло", code = 408 });
+                await context.Response.WriteAsJsonAsync(new { error = "Время ожидания истекло", code = 408, details = ex.Message });
             }
             catch (OperationCanceledException ex)
             {
                 _logger.LogWarning("Запрос отменён");
                 context.Response.StatusCode = 499;
-                await context.Response.WriteAsJsonAsync(new { error = "Запрос отменён", code = 499 });
+                await context.Response.WriteAsJsonAsync(new { error = "Запрос отменён", code = 499, details = ex.Message });
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                _logger.LogError(ex.Message);
+                context.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+                await context.Response.WriteAsJsonAsync(new { error = "Неавторизованный доступ", code = 401, details = ex.Message });
+            }
+            catch (ValidationException ex)
+            {
+                _logger.LogError(ex.Message);
+                context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                await context.Response.WriteAsJsonAsync(new{ error = "Данные не прошли валидацию", code = 400,  details = ex.Message });
             }
             catch (Exception ex)
             {
@@ -68,8 +81,9 @@ namespace LibraryAPI.Middlewares
 
                 var response = new
                 {
-                    errorCode = $"Ошибка. Код ошибки: {ex.HResult}",
-                    errorMessage = ex.Message
+                    error = "Ошибка на стороне сервера",
+                    code = ex.HResult,
+                    details = ex.Message
                 };
                 
                 await context.Response.WriteAsync(JsonSerializer.Serialize(response));
